2cca214fe5b12a9b00945cb60f6d3ce5cc4464c3
The ShadowZone <stuff@disrupt8.com>
2017-05-22 15:33:15 +0200

    blah

---
+++ Codecadamy/src/sz_javapkg_1/Variables.java
@@ -0,0 +1,17 @@
+package sz_javapkg_1;
+
+public class Variables {
+	public static void main(String[] args) {
+		
+		int myNumber = 6*7;
+		int modrest = 8%3;
+		boolean isFun = true;
+		char movieRating = 'A';
+		
+/*
+ * Variables will not be used in this file		
+ */
+		
+//		System.out.println(modrest);
+	}
+}
