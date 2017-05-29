3677d036186f0102aac16221ced26244c17ae874
The ShadowZone <stuff@disrupt8.com>
2017-05-29 13:20:14 +0200

    Examples from Codecadamy

---
+++ Codecadamy/src/sz_javapkg_1/Animal.java
@@ -0,0 +1,10 @@
+package sz_javapkg_1;
+
+class Animal {
+
+	public void checkStatus() {
+
+		System.out.println("Your pet is healthy and happy!");
+
+	}
+}
---
+++ Codecadamy/src/sz_javapkg_1/Dog.java
@@ -0,0 +1,37 @@
+package sz_javapkg_1;
+
+class Dog extends Animal{
+
+  int age;
+  
+public Dog(int dogsAge) {
+  
+  age = dogsAge;
+  
+}
+
+public void bark() {
+  System.out.println("Woof!");
+  
+}
+
+public void run(int feet) {
+ System.out.println("Your dog ran " + feet + " feet!");
+  
+}
+
+public int getAge() {
+  return age;
+  
+}
+  
+public static void main(String[] args) {
+
+    Dog spike = new Dog(13);
+  	spike.bark();
+  	spike.run(100);
+    int spikeAge = spike.getAge();
+  	System.out.println(spikeAge);
+  	spike.checkStatus();
+	}
+}
